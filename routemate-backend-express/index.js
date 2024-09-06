29f7b316a1068c08fda04fe0f6137341b170497b
banu-i <152266442+banu-i@users.noreply.github.com>
2024-09-06 19:39:49 +0200

    update : port zum testen angepasst
    localhost 3000  >> localhost 3030

--- routemate-backend-express/index.js
+++ routemate-backend-express/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3030;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hey Routmate!');
 });
 
 app.listen(port, () => {
